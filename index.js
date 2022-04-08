6fc5e706e93e1078cb3708ca4452bdc32c28d3c1
Mikhail <jackglowpetux@gmail.com>
2022-04-08 17:02:19 +0500

    Update

--- index.js
+++ index.js
@@ -16,28 +16,52 @@ const { pipeline } = require('stream');
 const pump = util.promisify(pipeline);
 
 const config = require('./config.json');
+const users = require("./users.json");
 
 fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'public'),
-  prefix: '/',
-})
+    root: path.join(__dirname, 'public'),
+    prefix: '/',
+});
+
+fastify.register(require("point-of-view"), {
+  engine: {
+    eta: require("eta"),
+  },
+});
 
 fastify.get('/', async (request, reply) => {
-    return "image server"
+    reply.view("private/index.ejs", {});
+});
+fastify.get('/setup', async (request, reply) => {
+    reply.view("private/chatterino.ejs", {
+        "key": request.query['key'] != undefined ? request.query['key'] : "YOURKEYHERE",
+    });
 });
 
 fastify.post('/uploadImage', async (req, reply) =>
 {
-    if (req.headers['authorization'] != config.AuthToken) { reply.code(401); return "Fuck you bitch!"; }
+    if (!(req.headers['authorization'] in users)) { reply.code(401); return "Fuck you bitch!"; }
+    const _user = users[req.headers['authorization']];
     const data = await req.file();
-    const fileName = `${generateFileName()}.png`;
-    await pump(data.file, fs.createWriteStream(path.join('public', fileName)));
+    const fileName = `${generateFileName(_user, 2)}.${getFileExt(data.filename)}`;
+    if (!fs.existsSync(path.join('public', _user['displayName']))) {
+        fs.mkdirSync(path.join('public', _user['displayName']));
+    }
+    await pump(data.file, fs.createWriteStream(path.join('public', _user['displayName'], fileName)));
 
-    return { "data": { "link": "https://boobs.pics/" + fileName, "deletehash": "test" } };
+    return { "data": { "link": "https://boobs.pics/" + _user['displayName'] + "/" + fileName, "deletehash": "test" } };
 });
 
-function generateFileName(length) {
-   return (Math.random() + 1).toString(36).substring(2);
+function generateFileName(u, length) {
+    var _gen = (Math.random() + 1).toString(36).substring(2);
+    if (fs.existsSync(path.join('public', u['displayName'], _gen)))
+        return generateFileName(length);
+   return _gen;
+}
+
+function getFileExt(filename)
+{
+    return filename.split('.').pop();
 }
 
 fastify.listen(3000).catch(() => { });
