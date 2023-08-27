af910a79a35459bf26b18fe8e85d3aeb372c3f48
mishashto <me@reze.moe>
2023-08-28 07:02:21 +0900

    Deprecate old upload method

---
+++ src/routes/deprecated_upload.js
@@ -0,0 +1,12 @@
+import { Route } from "http/routing";
+
+export default new Route("/uploadImage", "POST", async (request, reply) => {
+    reply.status(301);
+    reply.send({
+        "success": false,
+        "data": {
+            "message": "This route has been deprecated. Please use /api/private/uploads/new instead or download a new ShareX file through dashboard.",
+            "link": "This route has been deprecated. Please use /api/private/uploads/new instead or download a new ShareX file through dashboard."
+        }
+    });
+});
