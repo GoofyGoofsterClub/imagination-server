c1a34e3b504c959b3a9eb6e367b4f083da88a0f4
mishashto <me@reze.moe>
2023-08-28 01:15:25 +0900

    Update code to match the new shema

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 import { Route } from "http/routing";
 
 export default new Route("/", "GET", async (request, reply) => {
-    reply.send("Hello, world!");
+    reply.view("index.ejs", {
+        "domain": request.headers['host']
+    });
 });
