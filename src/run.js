c9ffb8330b96baa24369988f466d41daa8089a18
mishashto <me@reze.moe>
2023-08-28 00:20:03 +0900

    Add test endpoint

--- src/run.js
+++ src/run.js
@@ -1,1 +1,10 @@
-console.log("Test :)");
+const fastify = require('fastify')({ logger: true, bodyLimit: Number.MAX_SAFE_INTEGER });
+
+fastify.get('/', async (request, reply) => {
+    return { hello: 'world' };
+});
+
+fastify.listen({
+    host: '0.0.0.0',
+    port: process.env.HTTP_PORT
+}).catch((e) => { console.log(e); });
