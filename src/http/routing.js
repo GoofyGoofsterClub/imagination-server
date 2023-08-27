e552ea10df6d6130fb7432e6f7fc602cfe39dcca
mishashto <me@reze.moe>
2023-08-28 02:20:11 +0900

    Done for 2 hours, watching hp

--- src/http/routing.js
+++ src/http/routing.js
@@ -1,12 +1,13 @@
 import fs from "fs";
 import Output from "utilities/output";
-
+import GetRoutesResursively from "utilities/recursive";
 
 export default class HTTPRouting
 {
     static async RegisterRoutes(server)
     {
         const routes = fs.readdirSync(`${__dirname}/../routes`);
+        const api_routes = GetRoutesResursively(`${__dirname}/../api`);
         for(const route of routes)
         {
             const routeModule = await import(`${__dirname}/../routes/${route.split(".")[0]}`);
@@ -30,5 +31,20 @@ export class Route
     {
         this.db = server.db;
         await server.registerRoute(this.path, this.method, this.handler);
+    }
+}
+
+export class APIRoute
+{
+    constructor(method, handler)
+    {
+        this.method = method;
+        this.handler = handler;
+    }
+
+    async register(server, path)
+    {
+        this.db = server.db;
+        await server.registerRoute(path, this.method, this.handler);
     }
 }
