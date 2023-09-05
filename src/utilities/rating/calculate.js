affa5655fb4e05274a9edc44e4a6705f83eddc9e
mishashto <me@reze.moe>
2023-09-06 04:47:21 +0900

    Add a little bit of salt to the calculation

--- src/utilities/rating/calculate.js
+++ src/utilities/rating/calculate.js
@@ -2,7 +2,7 @@ export default function calculateRating(uploads, views, daysSinceUpload)
 {
     try
     {
-        let rating = parseFloat((views / uploads)-((views / uploads)*2*(daysSinceUpload/120))) ?? 0;
+        let rating = parseFloat((views / uploads)+(uploads*0.00035)-((views / uploads)*2*(daysSinceUpload/120))) ?? 0;
         if (isNaN(rating) || rating == Infinity || rating == -Infinity || rating == views)
             return 0;
         return rating;
