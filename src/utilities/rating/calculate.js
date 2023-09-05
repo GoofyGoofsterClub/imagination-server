4ac3a2bc8be4d5639c993b6420c9ad56b877d451
mishashto <me@reze.moe>
2023-09-06 03:41:39 +0900

    check for null

--- src/utilities/rating/calculate.js
+++ src/utilities/rating/calculate.js
@@ -2,7 +2,7 @@ export default function calculateRating(uploads, views, daysSinceUpload)
 {
     try
     {
-        let rating = parseFloat((views / uploads)-((views / uploads)*2*(daysSinceUpload/120)));
+        let rating = parseFloat((views / uploads)-((views / uploads)*2*(daysSinceUpload/120))) ?? 0;
         return rating;
     } catch
     {
