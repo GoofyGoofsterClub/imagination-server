1d1cc696e2c8cbaa503c9550b8df5a7f2ff16341
Mikhail <me@reze.moe>
2024-03-12 02:24:59 +0900

    API: Add api endpoint to change username

---
+++ src/api/private/session/changeusername.js
@@ -0,0 +1,45 @@
+import { APIRoute } from "http/routing";
+
+/*--includedoc
+
+@private false
+@needsauth true
+@adminonly false
+@params [(string) key, (string) new_name]
+@returns Returns true in success if request was processed.
+@returnexample { "success": true }
+Changes the display name of a user.
+WARNING: All previous uploads will still use old name, due to hashing.
+
+*/
+export default class ChangeUsername extends APIRoute
+{
+    constructor()
+    {
+        super("GET");
+    }
+
+    async call(request, reply)
+    {
+        if (!request.query.key || !request.query.new_name)
+        {
+            return {"success": false, "error": "One of the fields is missing."};
+        }
+
+        let doesExist = await this.db.checkDocumentExists("users", {
+            "key": request.query.key
+        });
+
+        await this.db.updateDocument("users", {
+            "key": request.query.key
+        }, {
+            "$set": {
+                "displayName": request.query.new_name 
+            }
+        });
+
+        return {
+            "success": true
+        };
+    }
+}
