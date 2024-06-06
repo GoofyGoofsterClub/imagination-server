e9e2592c95c07228b21e8b165fb6d71bef92abce
Mikhail <me@reze.moe>
2024-06-06 23:28:07 +0900

    API: Change upload file method to support service accounts

--- src/api/private/uploads/new.js
+++ src/api/private/uploads/new.js
@@ -29,26 +29,39 @@ export default class UploadsNewAPIRoute extends APIRoute
         // Needed for extension to work
         reply.header("Access-Control-Allow-Origin", "*");
 
+        let isServiceUpload = false;
+
         let _auth = await this._public.Authenticate(this.db, request.headers["authorization"]);
         if (!_auth)
         {
-            reply.status(401);
-            return reply.send({
-                "error": "Unauthorized"
+            _auth = await this.db.getDocument("services", {
+                "accessKey": request.headers['authorization']
             });
-        
+            console.log(_auth);
+            if (!_auth)
+            {
+                reply.status(401);
+                return reply.send({
+                    "error": "Unauthorized"
+                });
+            }
+            
+            isServiceUpload = true;
         }
 
-        let ratingResponse = await CheckRating(this.db, _auth.displayName);
-        if (!ratingResponse)
-        {   
-            reply.status(403);
-            return reply.send({
-                "error": "You are banned."
-            });
+        if (!isServiceUpload)
+        {
+            let ratingResponse = await CheckRating(this.db, _auth.displayName);
+            if (!ratingResponse)
+            {   
+                reply.status(403);
+                return reply.send({
+                    "error": "You are banned."
+                });
+            }
         }
 
-        if (_auth.isBanned)
+        if (!isServiceUpload && _auth.isBanned)
         {
             // if has ban duration and it passed then unban
             if (_auth.banDuration && _auth.banDuration < Date.now())
@@ -69,6 +82,14 @@ export default class UploadsNewAPIRoute extends APIRoute
             }
         }
 
+        if (isServiceUpload && _auth.disabled)
+        {
+            return {
+                "success": false,
+                "error": "This service is disabled. Please enable it in the dashboard or contact administrator."
+            }    
+        }
+
         const data = await request.file();
         const dataBuffer = await data.toBuffer();
         const datahash = await hashBuffer(dataBuffer.slice(0, 1024 * 1024));
@@ -83,7 +104,7 @@ export default class UploadsNewAPIRoute extends APIRoute
         }
 
         const existance = await this.db.getDocument("uploads", {
-            "uploader": hash(_auth.displayName),
+            "uploader": hash(_auth.displayName ?? _auth.internalKey),
             "hash": datahash
         });
 
@@ -92,15 +113,17 @@ export default class UploadsNewAPIRoute extends APIRoute
             reply.send({
                 "success": true,
                 "data": {
-                    "link": `https://${request.headers['host']}/${_auth.displayName}/${existance.filename}`
+                    "link": `https://${request.headers['host']}/${_auth.displayName ?? _auth.internalKey}/${existance.filename}`
                 }
             });
             return;
         }
 
+        let publicKeySlice = _auth.displayName ? hash(_auth.displayName.slice(0, 2)).slice(0, 2) + _auth.displayName.slice(0, 1) : hash(_auth.internalKey.slice(0, 2)).slice(0, 2) + _auth.internalKey.slice(0, 1);
+
         let ids = {
             "private": GeneratePrivateID(),
-            "public": GeneratePublicID(8, hash(_auth.displayName.slice(0, 2)).slice(0, 2) + _auth.displayName.slice(0, 1)),
+            "public": GeneratePublicID(8, publicKeySlice),
             "delete": GeneratePrivateID()
         };
         
@@ -113,7 +136,7 @@ export default class UploadsNewAPIRoute extends APIRoute
 
         let collection = await this.db.getCollection("uploads");
         await collection.insertOne({
-            "uploader": hash(_auth.displayName),
+            "uploader": hash(_auth.displayName ?? _auth.internalKey),
             "uploaderId": _auth._id,
             "actual_filename": ids.private,
             "original_filename": data.filename,
@@ -130,50 +153,51 @@ export default class UploadsNewAPIRoute extends APIRoute
         if (this._public.Ratelimits.length > 100)
             this._public.Ratelimits.shift();
         this._public.Ratelimits.push({
-            "userName": _auth.displayName,
+            "userName": _auth.displayName ?? _auth.internalKey,
             "timestamp": Date.now()
         });
 
-        let uploadsInLast10Seconds = this._public.Ratelimits.filter(x => x.userName == _auth.displayName && x.timestamp > Date.now() - 10000);
-        if (uploadsInLast10Seconds.length > 10 && !_auth.protected)
+        if (!isServiceUpload) // TO-DO(mishashto): Add another ratelimit for services.
         {
-            await this.db.updateDocument("users", {
-                "key": request.headers['authorization']
-                }, { "$set": {
-                    "isBanned": true,
-                    "banDuration": Date.now() + 60000,
-                    "banFieldModificationBy": "uwu"
-                    } });
-            reply.status(429);
-            return reply.send({
-                "error": "You are uploading too fast."
-            });
-        }
-
-        await addUpload(this.db, _auth.displayName);
-
-        if(_auth.isMonitored)
-        {
-            await this._public.ExternalLogging.Log(buildMessage(
-                request.headers['host'],
-                "info",
-                "A file has been uploaded.",
-                `A file has been uploaded by \`${_auth.displayName}\`:\n\`${data.filename}\``,
-                `https://${request.headers['host']}/${_auth.displayName}/${ids.public}`,
-                new Field("File ID", ids.public, false),
-                new Field("File Name", data.filename, true),
-                new Field("File Size", fileSizeInBytes, true),
-                new Field("File Extension", data.filename.split(".")[1], true),
-                new Field("File Mimetype", data.mimetype, true),
-                new Field("File Uploaded Through", request.headers['host'], true),
-                new Field("File Uploaded By", _auth.displayName, true)
-            ));
+            let uploadsInLast10Seconds = this._public.Ratelimits.filter(x => x.userName == _auth.displayName && x.timestamp > Date.now() - 10000);
+            if (uploadsInLast10Seconds.length > 10 && !_auth.protected)
+            {
+                await this.db.updateDocument("users", {
+                    "key": request.headers['authorization']
+                    }, { "$set": {
+                        "isBanned": true,
+                        "banDuration": Date.now() + 60000,
+                        "banFieldModificationBy": "uwu"
+                        } });
+                reply.status(429);
+                return reply.send({
+                    "error": "You are uploading too fast."
+                });
+            }
+            await addUpload(this.db, _auth.displayName);
+            if(_auth.isMonitored)
+            {
+                await this._public.ExternalLogging.Log(buildMessage(
+                    request.headers['host'],
+                    "info",
+                    "A file has been uploaded.",
+                    `A file has been uploaded by \`${_auth.displayName}\`:\n\`${data.filename}\``,
+                    `https://${request.headers['host']}/${_auth.displayName}/${ids.public}`,
+                    new Field("File ID", ids.public, false),
+                    new Field("File Name", data.filename, true),
+                    new Field("File Size", fileSizeInBytes, true),
+                    new Field("File Extension", data.filename.split(".")[1], true),
+                    new Field("File Mimetype", data.mimetype, true),
+                    new Field("File Uploaded Through", request.headers['host'], true),
+                    new Field("File Uploaded By", _auth.displayName, true)
+                ));
+            }
         }
 
         let shortenedUrl = null;
 
 
-        if (process.env.SHORTENER_URI && request.query['shorten'])
+        if (process.env.SHORTENER_URI && request.query['shorten'] && !isServiceUpload)
         {
             let shortenerRequest = await fetch(`https://${process.env.SHORTENER_URI}/api/link/shorten?key=${request.headers['authorization']}&link=https://${request.headers['host']}/${ids.public}`);
 
@@ -187,7 +211,7 @@ export default class UploadsNewAPIRoute extends APIRoute
         reply.send({
             "success": true,
             "data": {
-                "link": shortenedUrl ?? (request.query['useLegacyStyling'] ? `https://${request.headers['host']}/${_auth.displayName}/${ids.public}` : `https://${request.headers['host']}/${ids.public}`)
+                "link": shortenedUrl ?? (request.query['useLegacyStyling'] ? `https://${request.headers['host']}/${_auth.displayName ?? _auth.internalKey}/${ids.public}` : `https://${request.headers['host']}/${ids.public}`)
             }
         });
 
