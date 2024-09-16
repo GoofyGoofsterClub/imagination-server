34c02ec4dd0f5c836db7d4fb455f91bb61a4f732
Mikhail <me@reze.moe>
2024-09-17 07:47:18 +0900

    chore: remove unnecessary code from index

--- src/run.js
+++ src/run.js
@@ -2,11 +2,6 @@ import Output from "utilities/output";
 import HTTPServer from "http/server";
 import HTTPRouting from "http/routing";
 import DatabaseController from "db/db";
-import CalculateRatingWorker from "utilities/rating/calculationworker";
-import HeartbeatManager from "utilities/heartbeat";
-// These imports are for when there is no users.
-import { v4 as uuidv4 } from "uuid";
-import hash from "utilities/hash";
 
 Output.Log("Preparing the server...");
 
@@ -22,15 +17,13 @@ Output.Log("Preparing the server...");
 
     let amountOfUsers = await (await db.getCollection("users")).countDocuments();
 
-    setTimeout(() => CalculateRatingWorker(db, Output), 10000);
 
     Output.Log("Registering routes...");
     const server = new HTTPServer(db);
 
     let isInitialSetup = !(await db.checkDocumentExists("globals", { "field": "_initialSetup" }));
-    
-    if (amountOfUsers < 1)
-    {
+
+    if (amountOfUsers < 1) {
         Output.Warn("sys", `No users found in the database. Please complete the initial user setup.`);
     }
 
