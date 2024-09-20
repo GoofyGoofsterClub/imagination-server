582dd11e6eaaa3de6cc8794b091e3a885dc984bd
Mikhail <me@reze.moe>
2024-09-20 17:13:16 +0900

    refactor(api): file deletion also uses postgres now

--- src/api/private/uploads/delete.js
+++ src/api/private/uploads/delete.js
@@ -19,21 +19,20 @@ export default class DeleteUploadAPIRoute extends APIRoute {
     }
 
     async call(request, reply, server) {
-        let _auth = await server.server._public.Authenticate(server.odb, hash(request.query.key));
-        if (!_auth) {
-            reply.status(401);
-            return reply.send({
-                "error": "Unauthorized"
-            });
+        let doesUserExist = await server.db.doesUserExistByAccessKey(hash(request.query.key));
 
-        }
+        if (!doesUserExist)
+            return {
+                "success": false,
+                "error": "Invalid key."
+            };
 
-        if (_auth.isBanned) {
-            reply.status(403);
-            return reply.send({
-                "error": "You are banned."
-            });
-        }
+        let user = await server.db.findUserByAccessKey(hash(request.query.key));
+
+        if (user.banned) return {
+            "success": false,
+            "error": "You are banned."
+        };
 
         if (!request.query.filename)
             return {
@@ -43,17 +42,13 @@ export default class DeleteUploadAPIRoute extends APIRoute {
 
         if (request.query.filename == "*") {
             // delete all of theirs
-            let collection = await server.odb.getCollection("uploads");
-            let uploads = await collection.find({
-                "uploader": hash(_auth.displayName)
-            }).toArray();
+            let collection = await server.db.query(`SELECT * FROM uwuso.uploads WHERE uploader_id = $1::bigint`, [user.id]);
+            let uploads = collection.rows;
 
-            await collection.deleteMany({
-                "uploader": hash(_auth.displayName)
-            });
+            await server.db.query(`DELETE FROM uwuso.uploads WHERE uploader_id = $1::bigint`, [user.id]);
 
             for (let upload of uploads) {
-                fs.unlinkSync(`${__dirname}/../../../../privateuploads/${upload.actual_filename}`);
+                fs.unlinkSync(`${__dirname}/../../../../privateuploads/${upload.disk_filename}`);
             }
 
             return {
@@ -61,30 +56,23 @@ export default class DeleteUploadAPIRoute extends APIRoute {
             };
         }
 
-        let doesExist = await server.odb.checkDocumentExists("uploads", {
-            "filename": request.query.filename
-        });
+        let fileInfo = await server.db.query(`SELECT * FROM uwuso.uploads WHERE filename = $1::text`, [request.query.filename]);
 
-        if (!doesExist)
+        if (fileInfo.rows.length < 1)
             return {
                 "success": false,
                 "error": "File does not exist."
             };
 
-        let upload = await server.odb.getDocument("uploads", {
-            "filename": request.query.filename
-        });
+        fileInfo = fileInfo.rows[0];
 
-        if (upload.uploader !== hash(_auth.displayName))
+        if (fileInfo.uploader_id !== user.id)
             return {
                 "success": false,
                 "error": "You do not own this file."
             };
 
-        let collection = await server.odb.getCollection("uploads");
-        await collection.deleteOne({
-            "filename": request.query.filename
-        });
+        await server.db.query(`DELETE FROM uwuso.uploads WHERE uploader_id = $1::bigint AND filename = $2::text`, [user.id, request.query.filename]);
 
         fs.unlinkSync(`${__dirname}/../../../../privateuploads/${upload.actual_filename}`);
 
